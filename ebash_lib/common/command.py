3cd7f88cf85f7b2067ec6b38fb76209409af8a4f
Wizzerinus <quillinde@gmail.com>
2025-05-11 17:33:50 +0300

    make command extendable

--- ebash_lib/common/command.py
+++ ebash_lib/common/command.py
@@ -1,9 +1,9 @@
 import abc
 import dataclasses
-from collections.abc import Iterable
-from typing import final, override
-import os
 import sys
+from collections.abc import Iterable
+from typing import Callable, ClassVar, final, override
+
 from ebash_lib.common.context import Context
 
 
@@ -12,72 +12,91 @@ class MetaCommand(abc.ABC):
     def __call__(self, ctx: Context) -> Context: ...
 
 
+@final
+class CommandRunner(MetaCommand):
+    registered_commands: ClassVar[dict[str, Callable[[list[str], Context], Context]]] = {}
 
+    @classmethod
+    def register(cls, command: Callable[[list[str], Context], Context]):
+        cls.registered_commands[command.__name__] = command
+        return command
 
-class CommandRunner:
     def __init__(self, args: list[str]):
+        # args is never empty
         self.args = args
 
     @override
-    def __call__(self, ctx: dict) -> dict:
-        if not self.args:
-            return ctx
-
+    def __call__(self, ctx: Context) -> Context:
         command = self.args[0]
 
-        if command == "echo":
-            print(" ".join(self.args[1:]))
-
-        elif command == "cat":
-            if len(self.args) < 2:
-                print("Usage: cat <file>")
-            else:
-                try:
-                    with open(self.args[1], 'r') as file:
-                        print(file.read())
-                except FileNotFoundError:
-                    print(f"Error: File '{self.args[1]}' not found.")
-
-        elif command == "wc":
-            if len(self.args) < 2:
-                print("Usage: wc <file>")
-            else:
-                try:
-                    with open(self.args[1], 'r') as file:
-                        content = file.read()
-                        lines = content.split('\n')
-                        words = content.split()
-                        chars = len(content)
-                        print(f"{len(lines)} {len(words)} {chars} {self.args[1]}")
-                except FileNotFoundError:
-                    print(f"Error: File '{self.args[1]}' not found.")
-
-        elif command == "pwd":
-            print(os.getcwd())
-
-        elif command == "exit":
-            sys.exit(0)
-
-        elif command == "grep":
-            if len(self.args) < 3:
-                print("Usage: grep <pattern> <file>")
-            else:
-                try:
-                    with open(self.args[2], 'r') as file:
-                        for line in file:
-                            if self.args[1] in line:
-                                print(line.strip())
-                except FileNotFoundError:
-                    print(f"Error: File '{self.args[2]}' not found.")
-
+        if command in self.registered_commands:
+            return self.registered_commands[command](self.args[1:], ctx)
         else:
             raise NotImplementedError(f"Command '{command}' not supported.")
-
-        return ctx
 
     @override
     def __eq__(self, value: object, /) -> bool:
         return isinstance(value, CommandRunner) and value.args == self.args
+
+
+@CommandRunner.register
+def echo(args: list[str], ctx: Context):
+    return Context(0, ctx.workdir, [" ".join(args)], ctx.stderr)
+
+
+@CommandRunner.register
+def cat(args: list[str], ctx: Context):
+    if len(args) < 1:
+        return ctx.with_error(2, "Usage: cat <file>")
+    else:
+        try:
+            with open(args[0], "r") as file:
+                return Context(0, ctx.workdir, file.readlines(), ctx.stderr)
+        except FileNotFoundError:
+            return ctx.with_error(1, f"Error: File '{args[0]}' not found.")
+
+
+@CommandRunner.register
+def wc(args: list[str], ctx: Context):
+    if len(args) < 1:
+        return ctx.with_error(2, "Usage: wc <file>")
+    else:
+        try:
+            with open(args[0], "r") as file:
+                content = file.read()
+                lines = content.split("\n")
+                words = content.split()
+                chars = len(content)
+                output = f"{len(lines)}\t{len(words)}\t{chars}\t{args[0]}"
+                return Context(ctx.return_code, ctx.workdir, [output], ctx.stderr)
+        except FileNotFoundError:
+            return ctx.with_error(1, f"Error: File '{args[0]}' not found.")
+
+
+@CommandRunner.register
+def pwd(_args: list[str], ctx: Context):
+    return Context(0, ctx.workdir, [ctx.workdir], ctx.stderr)
+
+
+@CommandRunner.register
+def exit(_args: list[str], _ctx: Context):
+    sys.exit(0)
+
+
+@CommandRunner.register
+def grep(args: list[str], ctx: Context):
+    if len(args) < 2:
+        return ctx.with_error(2, "Usage: grep <pattern> <file>")
+    else:
+        out: list[str] = []
+        try:
+            with open(args[1], "r") as file:
+                for line in file:
+                    if args[0] in line:
+                        print(line.strip())
+            return Context(0, ctx.workdir, out, ctx.stderr)
+        except FileNotFoundError:
+            return ctx.with_error(1, f"Error: File '{args[1]}' not found.")
 
 
 @final
--- ebash_lib/common/context.py
+++ ebash_lib/common/context.py
@@ -4,4 +4,9 @@ import dataclasses
 @dataclasses.dataclass(frozen=True)
 class Context:
     return_code: int
+    workdir: str
     stdout: list[str]
+    stderr: list[str]
+
+    def with_error(self, error_code: int, error_message: str):
+        return Context(error_code, self.workdir, [], self.stderr + [error_message])
